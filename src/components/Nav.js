e72a066266899a43c026ee7ed17e5d872055b95c
David Sypien <davidsypien@gmail.com>
2021-11-10 18:13:24 -0500

    fixed warnings in Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,8 +3,7 @@ import React, { Component } from "react"
 class Nav extends Component {
    render(){
       return (
-         <nav class="navbar navbar-light bg-dark">   
-                    
+         <nav className="navbar navbar-light bg-dark">                       
          </nav>
       )      
    }
