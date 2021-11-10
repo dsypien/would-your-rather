90824b1659078dc22e33d459b3797664363f1f54
David Sypien <davidsypien@gmail.com>
2021-11-10 18:00:54 -0500

    added bootstrap packages and created nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Nav extends Component {
+   render(){
+      return (
+         <nav class="navbar navbar-light bg-dark">   
+                    
+         </nav>
+      )      
+   }
+}
+
+export default Nav
