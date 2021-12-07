9eddc571e17d52e6e53d8d3aa5d6d6f06b2a7d79
David Sypien <davidsypien@gmail.com>
2021-12-06 21:26:59 -0500

    List id's of answered and unanswered questions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react' 
+import { connect } from 'react-redux'
 
 class Home extends Component {
    render () {
+      const { authedUser, questions } = this.props
+      const answers = authedUser ? Object.keys(authedUser.answers) : []
+
+      const answeredQuestions = Object.keys(questions)
+                                      .filter( (e) => answers.includes(e))
+                                      .reduce( (ary, key) => { 
+                                         let question = questions[key]  
+                                         question.answer = answers[key]
+                                         ary.push(question)
+                                         return ary
+                                       }, [])
+
+      const unansweredQuestions = Object.keys(questions)
+                                        .filter( (e) => !answers.includes(e))
+                                        .reduce( (ary, key) => { 
+                                          let question = questions[key]  
+                                          ary.push(question)
+                                          return ary
+                                       }, [])
+
       return (
          <div>
-            Home
+            <h2>Answered</h2>
+            <ul>
+               {answeredQuestions.map( ans => (
+                  <li>{ans.id}</li>
+               ))} 
+            </ul>
+            <h2>Unanswered</h2>
+            <ul>
+               {unansweredQuestions.map( ans => (
+                     <li>{ans.id}</li>
+               ))} 
+            </ul>
          </div>
       )
    }
 }
 
-export default Home
+function mapStateToProps ( {questions, users, authedUser} ){
+   return {
+      questions,
+      authedUser : users[authedUser],
+      answeredQuestions: questions[authedUser]
+   }
+} 
+
+export default connect(mapStateToProps)(Home)
