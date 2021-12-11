4a71fd49a7c93d6de3eef1449d502b10e4852fa8
David Sypien <davidsypien@gmail.com>
2021-12-11 17:25:43 -0500

    Added some styling to poll results

--- src/components/PollDetailsOptionResult.js
+++ src/components/PollDetailsOptionResult.js
@@ -1,13 +1,23 @@
 import React from "react"
+import { ProgressBar } from "react-bootstrap"
 
 function PollDetailsOptionResult (params) {
    const { option, selectedCount, totalCount } = params
+   const percentSelected = 100 * (selectedCount / totalCount)
 
    return (
-      <div className="card poll bg-light mb-3">
-         <div>Would rather {option.text}</div>
-         <div>{ 100 * (selectedCount / (totalCount))}%</div>
-         <div>{`${selectedCount} out of ${totalCount} votes`}</div>
+      <div className="card poll-option bg-light mb-3">
+         <div className="poll-option-text mb-3">
+            Would rather {option.text}
+         </div>
+         <ProgressBar 
+            className="mb-3"
+            now={percentSelected} 
+            label={`${percentSelected}%`}
+            variant="info" />
+         <div className="poll-option-text">
+            {`${selectedCount} out of ${totalCount} votes`}
+         </div>
       </div>
    )
 }
