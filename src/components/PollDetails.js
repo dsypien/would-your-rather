71064a52e2ed576b72ade739c93701596431794c
David Sypien <davidsypien@gmail.com>
2021-12-17 06:43:22 -0500

    go to login page from poll page if user is not signed in

--- src/components/PollDetails.js
+++ src/components/PollDetails.js
@@ -1,20 +1,28 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { connect } from "react-redux"
 import { useParams } from "react-router-dom"
 import PollDetailsQuestion from "./PollDetailsQuestion"
 import PollDetailsResult from "./PollDetailsResult"
+import { useNavigate } from "react-router"
 
 function PollDetails (props) {
    const { id } = useParams()
    const { questions, users, authedUser } = props
    const question = questions[id]
+   const navigate = useNavigate()
+
+   useEffect(() => {
+      if(!authedUser){
+         navigate("/")
+      }
+   }, [navigate, authedUser])
 
    let pollBody 
 
-   if(question.id in users[authedUser].answers) {
+   if (question && question.id in users[authedUser].answers) {
       pollBody = <PollDetailsResult question={question} />  
    }
-   else {
+   else if (authedUser) {
       pollBody = <PollDetailsQuestion question={question}/>
    }
 
@@ -22,11 +30,11 @@ function PollDetails (props) {
       <div>
          <div className="card poll bg-light mt-3">
             <div className="card-header">
-               { `${users[question.author].name} asks:`}
+               { authedUser && `${users[question.author].name} asks:`}
             </div>
             <div className="flex-container">
                <div className=" avatar-container">
-                  <img alt="avatar" className="avatar-lg" src={users[question.author].avatarURL} />
+                  {authedUser && <img alt="avatar" className="avatar-lg" src={users[question.author].avatarURL} />}                  
                </div>
                <div className="card-body">                  
                   {pollBody}
