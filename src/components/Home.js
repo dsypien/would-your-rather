c9999fd9b67907a6e1c25ce192bd8e2aded3cb62
David Sypien <davidsypien@gmail.com>
2021-12-16 20:30:56 -0500

    redirect user to login page, if user types something into address bar on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,35 @@
-import React, { Component } from 'react' 
+import React, { useEffect } from 'react' 
 import PollList from './PollList'
 import { connect } from 'react-redux'
 import {Tab, Tabs } from 'react-bootstrap'
+import { useNavigate } from 'react-router'
 
-class Home extends Component {
-   render () {
-      const { users, answeredQuestions, unansweredQuestions } = this.props
-                   
-      return (
-         <div>
-            <Tabs
-               id="controlled-tab-example"
-               defaultActiveKey="unanswered"
-               transition={false}
-               className="mb-3">
-               <Tab eventKey="unanswered" title="Unanswered">
-                  <PollList questions={unansweredQuestions} users={users} name="unanswered"/>
-               </Tab>   
-               <Tab eventKey="answered" title="Answered">
-                  <PollList questions={answeredQuestions} users={users} name="answered"/>
-               </Tab>                           
-            </Tabs>
-         </div>
-      )
-   }
+function Home (props) {
+   const { users, answeredQuestions, unansweredQuestions, authedUser } = props
+   const navigate = useNavigate()
+
+   useEffect(() => {
+      if (!authedUser){
+         navigate("/")
+      }
+   }, [navigate, authedUser])
+                  
+   return (
+      <div>
+         <Tabs
+            id="controlled-tab-example"
+            defaultActiveKey="unanswered"
+            transition={false}
+            className="mb-3">
+            <Tab eventKey="unanswered" title="Unanswered">
+               <PollList questions={unansweredQuestions} users={users} name="unanswered"/>
+            </Tab>   
+            <Tab eventKey="answered" title="Answered">
+               <PollList questions={answeredQuestions} users={users} name="answered"/>
+            </Tab>                           
+         </Tabs>
+      </div>
+   )
 }
 
 function mapStateToProps ( {questions, users, authedUser} ){
@@ -31,6 +37,7 @@ function mapStateToProps ( {questions, users, authedUser} ){
    const answers = authedUser ? Object.keys(user.answers) : []
 
    return {
+      authedUser,
       users,
       answeredQuestions: Object.keys(questions)
                                .filter( (e) => answers.includes(e))
