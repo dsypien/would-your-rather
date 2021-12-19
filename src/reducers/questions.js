b159a0a68a68e2425de49f3379b0fd7392fceffc
David Sypien <davidsypien@gmail.com>
2021-12-18 23:10:37 -0500

    update question state when updating user answer

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,5 @@
 import { RECEIVE_QUESTIONS, ADD_QUESTION } from "../actions/questions"
+import { SAVE_USER_ANSWER } from "../actions/users"
 
 export default function questions(state = {}, action){
    switch(action.type){
@@ -12,6 +13,8 @@ export default function questions(state = {}, action){
             ...state,
             [action.question.id]: action.question
          }
+      case SAVE_USER_ANSWER:  
+         return state
       default:
          return state
    }
