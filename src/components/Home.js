d19a280c08a60effcf906297c3737832343ff38a
David Sypien <davidsypien@gmail.com>
2021-12-18 20:36:42 -0500

    sort polls by most recently created at top

--- src/components/Home.js
+++ src/components/Home.js
@@ -46,14 +46,16 @@ function mapStateToProps ( {questions, users, authedUser} ){
                                   question.answer = answers[key]
                                   ary.push(question)
                                   return ary
-                               }, []),
+                               }, [])
+                               .sort((a, b) => b.timestamp - a.timestamp),
       unansweredQuestions: Object.keys(questions)
                                  .filter( (e) => !answers.includes(e))
                                  .reduce( (ary, key) => { 
-                                 let question = questions[key]  
-                                 ary.push(question)
-                                 return ary
+                                    let question = questions[key]  
+                                    ary.push(question)
+                                    return ary
                                  }, [])
+                                 .sort((a, b) => b.timestamp - a.timestamp),
    }
 } 
 
